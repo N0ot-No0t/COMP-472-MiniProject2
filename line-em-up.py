f261bd4702240fddae4f1479dfc8d2eba262ec55
unknown <sustaccosmin544@hotmail.ca>
2021-11-15 03:13:07 -0500

    Fix input. Fix diagonal evaluation to account for all possible diagonals of a matrix

--- line-em-up.py
+++ line-em-up.py
@@ -9,24 +9,42 @@ class Game:
     ALPHABETA = 1
     HUMAN = 2
     AI = 3
+    DEV = True
     
     def __init__(self, recommend = True):
         self.initialize_game()
         self.recommend = recommend
         
     def initialize_game(self):
-        self.dimension = self.get_dimension_input("Enter your value for \"n\", where \"n\" will be the dimension (n x n) of the board: ")
-        self.current_state = [['.' for col in range(self.dimension)] for row in range(self.dimension)]
-        self.current_state = np.asarray(self.current_state)
-        self.nb_blocs = self.get_num_blocs_input("Enter your value for \"b\", where \"b\" is the number of blocs present on the board: ")
-        self.win_size = self.get_win_size_input("Enter your value for \"s\", where \"s\" is the number of required pieces to win: ")
-        self.set_blocks() #add block to game board
-        self.max_time = self.get_integer_input("Enter the max allowed time “t” (in seconds) for program to return a move: ")
-        self.algo = self.get_algo_input("Enter false to do minimax or true to do alphabeta: ")
-        self.player_x = self.get_player_input("Enter the word 'human' if you want player X to be a human. Enter 'AI' if you want player X to be under AI control: ")
-        self.player_o = self.get_player_input("Enter the word 'human' if you want player O to be a human. Enter 'AI' if you want player O to be under AI control: ")
-        self.depth_x = self.get_integer_input("Enter the max depth for player X: ")
-        self.depth_o = self.get_integer_input("Enter the max depth for player O: ")
+        if self.DEV:
+            self.dimension = 5
+            self.current_state = [['.' for col in range(self.dimension)] for row in range(self.dimension)]
+            self.current_state = np.asarray(self.current_state)
+            self.nb_blocs = 4
+            self.win_size = 4
+            self.current_state[2][2] = '#'
+            self.current_state[1][0] = '#'
+            self.current_state[0][4] = '#'
+            self.current_state[4][3] = '#'
+            self.max_time = 2
+            self.algo = self.MINIMAX
+            self.player_x = self.AI
+            self.player_o = self.AI
+            self.depth_x = 2
+            self.depth_o = 2
+        else:
+            self.dimension = self.get_dimension_input("Enter your value for \"n\", where \"n\" will be the dimension (n x n) of the board: ")
+            self.current_state = [['.' for col in range(self.dimension)] for row in range(self.dimension)]
+            self.current_state = np.asarray(self.current_state)
+            self.nb_blocs = self.get_num_blocs_input("Enter your value for \"b\", where \"b\" is the number of blocs present on the board: ")
+            self.win_size = self.get_win_size_input("Enter your value for \"s\", where \"s\" is the number of required pieces to win: ")
+            self.set_blocks() #add block to game board
+            self.max_time = self.get_integer_input("Enter the max allowed time \"t\" (in seconds) for program to return a move: ")
+            self.algo = self.get_algo_input("Enter false to do minimax or true to do alphabeta: ")
+            self.player_x = self.get_player_input("Enter the word 'human' if you want player X to be a human. Enter 'AI' if you want player X to be under AI control: ")
+            self.player_o = self.get_player_input("Enter the word 'human' if you want player O to be a human. Enter 'AI' if you want player O to be under AI control: ")
+            self.depth_x = self.get_integer_input("Enter the max depth for player X: ")
+            self.depth_o = self.get_integer_input("Enter the max depth for player O: ")
         # Player X always plays first
         self.player_turn = 'X'
 
@@ -57,11 +75,13 @@ class Game:
         while True:
             try:
                 win_size = int(input(prompt))
-                if win_size >= 3 and win_size <=self.dimension:
+                print("Inputted "+str(win_size))
+                if win_size >= 3 and win_size <= self.dimension:
                     return win_size
                 print(f"Invalid input. The winning line-up size must be between 3 and {self.dimension}. Please try again!")
             except ValueError:
                 print('Invalid input. Please try again!')
+                break
 
     def get_integer_input(self, prompt):
         while True:
@@ -98,10 +118,10 @@ class Game:
         blocs = self.nb_blocs
         while (blocs > 0):
             print("Please input the coordinates of the bloc: ")
-            x_coordinate = int(input('enter the x (row) coordinate: '))
-            y_coordinate = int(input('enter the y (column) coordinate: '))
+            x_coordinate = int(transform_input_to_int(input('enter the x (row) position (A,B,C,D...): ')))
+            y_coordinate = int(input('enter the y (column) coordinate (0,1,2,3...): '))
             if self.is_valid(x_coordinate, y_coordinate):
-                self.current_state[x_coordinate][y_coordinate] = '*'
+                self.current_state[x_coordinate][y_coordinate] = '#'
                 blocs-=1
             else:
                 print('Invalid coordinate. Please Try again!')
@@ -132,7 +152,7 @@ class Game:
     def is_end(self):
 
         # Vertical win
-        for i in range(int(self.dimension)):
+        for i in range(self.dimension):
             sequence = "".join(column(i, self.current_state)).strip()
             #X wins
             if re.search("X{"+str(self.win_size)+",}",sequence):
@@ -142,7 +162,7 @@ class Game:
                 return 'O'
 
         # Horizontal win
-        for i in range(int(self.dimension)):
+        for i in range(self.dimension):
             sequence = "".join(row(i, self.current_state)).strip()
             #X wins
             if re.search("X{"+str(self.win_size)+",}",sequence):
@@ -151,26 +171,18 @@ class Game:
             elif re.search("O{"+str(self.win_size)+",}",sequence):
                 return 'O'
 
-        # Main diagonal win
-        sequence = "".join(diagonal1(self.current_state)).strip()
-        #X wins
-        if re.search("X{"+str(self.win_size)+",}",sequence):
-            return 'X'
-        #O wins
-        elif re.search("O{"+str(self.win_size)+",}",sequence):
-            return 'O'      
-
-        # Second diagonal win
-        sequence = "".join(diagonal2(self.current_state)).strip()
-        #X wins
-        if re.search("X{"+str(self.win_size)+",}",sequence):
-            return 'X'
-        #O wins
-        elif re.search("O{"+str(self.win_size)+",}",sequence):
-            return 'O'  
+        # Any diagonal win
+        for i in range(self.dimension+self.dimension+1):
+            sequence = "".join(diagonal(i,self.current_state)).strip()
+            #X wins
+            if re.search("X{"+str(self.win_size)+",}",sequence):
+                return 'X'
+            #O wins
+            elif re.search("O{"+str(self.win_size)+",}",sequence):
+                return 'O'      
 
         # Is whole board full?
-        for i in range(0, int(self.dimension)):
+        for i in range(0, self.dimension):
             for j in range(0, int(self.dimension)):
                 # There's an empty field, we continue the game
                 if (self.current_state[i][j] == '.'):
@@ -343,21 +355,26 @@ class Game:
             self.switch_player()
 
     def e2(self):
-        sequence = ''
         score = 0
         #for each row and column
-        for i in range(int(self.dimension)):
+        for i in range(self.dimension):
             col_sequence = "".join(column(i, self.current_state)).strip()
             row_sequence = "".join(row(i, self.current_state)).strip()
             for j in range(int(self.win_size)):
-                if re.search("O{"+str(int(self.win_size)-j)+",}",col_sequence):
-                    score+=(int(self.win_size)-j)*10**(int(self.win_size)-j)
-                elif re.search("X{"+str(int(self.win_size)-j)+",}",col_sequence):
-                    score-=(int(self.win_size)-j)*10**(int(self.win_size)-j)
-                if re.search("O{"+str(int(self.win_size)-j)+",}",row_sequence):
-                    score+=(int(self.win_size)-j)*10**(int(self.win_size)-j)
-                elif re.search("X{"+str(int(self.win_size)-j)+",}",row_sequence):
-                    score-=(int(self.win_size)-j)*10**(int(self.win_size)-j)
+                if re.search("O{"+str(self.win_size-j)+",}",col_sequence):
+                    score+=(self.win_size-j)*10**(self.win_size-j)
+                elif re.search("X{"+str(self.win_size-j)+",}",col_sequence):
+                    score-=(self.win_size-j)*10**(self.win_size-j)
+                if re.search("O{"+str(self.win_size-j)+",}",row_sequence):
+                    score+=(self.win_size-j)*10**(self.win_size-j)
+                elif re.search("X{"+str(self.win_size-j)+",}",row_sequence):
+                    score-=(self.win_size-j)*10**(self.win_size-j)
+        for i in range(2*self.dimension+1):
+            diag_sequence = "".join(diagonal(i, self.current_state)).strip()
+            if re.search("O{"+str(self.win_size-j)+",}",diag_sequence):
+                    score+=(self.win_size-j)*10**(self.win_size-j)
+            elif re.search("X{"+str(self.win_size-j)+",}",diag_sequence):
+                score-=(self.win_size-j)*10**(self.win_size-j)
         return score
             
 
@@ -371,17 +388,21 @@ def transform_input_to_char(int_val):
     letters = ['A','B','C','D','E','F','G','H','I','J',]
     return letters[int_val]
 
+#Return i_th col
 def column(i, state):
     return [col[i] for col in state.T]
 
+#Return i_th row
 def row(i, state):
     return [row[i] for row in state]
 
-def diagonal1(state):
-    return state.diagonal()
+#It just works, don't question it. Returns the n_th diagonal.
+#Considering a NxN matrix has 2N+1 diagonals, that will be the amount of diagonals that can be acquired.
+def diagonal(n,state):
+    diags = [state[::-1,:].diagonal(i) for i in range(-3,4)]
+    diags.extend(state.diagonal(i) for i in range(3,-4,-1))
+    return diags[n].tolist()
 
-def diagonal2(state):
-    return np.fliplr(state).diagonal()
 
 def main():
     g = Game(recommend=True)
